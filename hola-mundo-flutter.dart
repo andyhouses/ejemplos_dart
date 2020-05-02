67d9438dded15bfcb603f10e7f4e582efa643795
Edwin Sandoval <sandovaledwin@Edwins-MacBook-Pro.local>
2020-05-02 16:17:43 -0500

    Hola Mundo Flutter

---
+++ hola-mundo-flutter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Container(
+        color: Colors.amberAccent,
+        child:  Text(
+          'Hello, world!',
+          textDirection: TextDirection.ltr,
+        ),
+      )
+    ),
+  );
+}
