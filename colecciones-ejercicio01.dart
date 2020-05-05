31a28e7046721f24725bdf563bd57020cae82a46
Andrea Parkhurst <andrea.parkhurst@alumnos.udg.mx>
2020-05-05 16:45:51 -0500

    colecciones parte 1

---
+++ colecciones-ejercicio01.dart
@@ -0,0 +1,10 @@
+// Colecciones List
+main(List<String> arguments) {
+  List<String> colors; //declarando lista
+  colors = ['blue','pink','white'];
+
+  //agregar
+colors.add('green');
+
+  print (colors);
+}
