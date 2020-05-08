d036f45dfd098bf9bc0c316e8d7023fbcd389656
Andrea Parkhurst <andrea.parkhurst@alumnos.udg.mx>
2020-05-08 15:28:29 -0500

    declarando map

---
+++ map.dart
@@ -0,0 +1,23 @@
+// Maps
+main(List<String> arguments) {
+  //utilizando dynamic en vez de String se puede
+  //usar cualquier valor tipo de valor
+  Map<int, String> colores; //declarando objeto tipo map
+  colores = {1: 'azul', 2: 'blanco'};
+  print(colores);
+
+  // Agregar otro color
+  colores[3]='rosa';
+  print(colores);
+
+  //eliminar un color
+  colores.remove(2);
+  print(colores);
+
+  // para saber si esta vacio o no
+  print(colores.isEmpty);
+  //saber la longitud
+  print(colores.length);
+  print ("En la lista tenemos: ${colores.length} colores");
+
+}
